4fba788da68c49c8b6a51a18084a67a46bc387ea
Wesley Monten <wesleymonten123@gmail.com>
2020-01-22 15:01:33 +0100

    template crud methodes go client

--- Dgraph/Go Client/main.go
+++ Dgraph/Go Client/main.go
@@ -44,36 +44,59 @@ func main() {
 
 	ctx := context.Background()
 
-	CreateQuery(ctx, dg)
-	SearchQuery(ctx, dg)
-	// DeleteQuery(ctx, dg, "0x21")
-	SearchQuery(ctx, dg)
+	// CreateUserQuery(ctx, dg)
+	// DeleteUserQuery(ctx, dg, "0x3a")
+	// SearchAllUsersQuery(ctx, dg)
+	// SearchUserQuery(ctx, dg, "0x37")
+	DeleteUserQuery(ctx, dg, "0x32")
+	// UpdateUserQuery(ctx, dg, "0x32")
 }
 
-// DeleteQuery deletes a node
-func DeleteQuery(ctx context.Context, dg *dgo.Dgraph, id string) {
+// UpdateUserQuery updates a usernode
+func UpdateUserQuery(ctx context.Context, dg *dgo.Dgraph, id string) {
 
-	// variables := map[string]string{"$id": id}
+	pb, err := json.Marshal(User{Uid: id, Name: "Blueface", Role: "VAPEGOD"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req := &api.Request{CommitNow: true}
+
+	mu := &api.Mutation{SetJson: pb}
+	mu.SetJson = pb
+	req.Mutations = []*api.Mutation{mu}
+
+	if _, err := dg.NewTxn().Do(ctx, req); err != nil {
+		log.Fatal(err)
+	}
 
 	variables := map[string]string{"$id": id}
-	q := `query createUser($id: string){
-		create(func: uid($id)){
+	q := `query getUser($id: string){
+		get(func: uid($id)){
 			name
 			role
-			voertuig {
-				type
-				latitude
-				longitude
-				service {
-					needsservice
-				}
-			}
+			voertuig
+		}
+	}`
+
+	resp, err := dg.NewReadOnlyTxn().QueryWithVars(ctx, q, variables)
+	if err != nil || resp == nil {
+		log.Fatal(err)
+	}
+}
+
+// DeleteConnectionBetweenUserAndVehicleQuery deletes the edge between a usernode and a vehiclenode
+func DeleteConnectionBetweenUserAndVehicleQuery(ctx context.Context, dg *dgo.Dgraph, id string) {
+	variables := map[string]string{"$id": id}
+	q := `query getUser($id: string){
+		get(func: uid($id)){
+			name
+			role
 		}
 	}`
 
 	mu := &api.Mutation{}
 	dgo.DeleteEdges(mu, id, "voertuig")
-	dgo.DeleteEdges(mu, id, "name", "role")
 	mu.CommitNow = true
 
 	resp, err := dg.NewTxn().Mutate(ctx, mu)
@@ -88,8 +111,35 @@ func DeleteQuery(ctx context.Context, dg *dgo.Dgraph, id string) {
 
 }
 
-// CreateQuery creates a node
-func CreateQuery(ctx context.Context, dg *dgo.Dgraph) {
+// DeleteUserQuery deletes a node
+func DeleteUserQuery(ctx context.Context, dg *dgo.Dgraph, id string) {
+
+	variables := map[string]string{"$id": id}
+	q := `query getUser($id: string){
+		get(func: uid($id)){
+			name
+			role
+		}
+	}`
+
+	mu := &api.Mutation{}
+	dgo.DeleteEdges(mu, id, "name", "role", "voertuig")
+	mu.CommitNow = true
+
+	resp, err := dg.NewTxn().Mutate(ctx, mu)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err = dg.NewTxn().QueryWithVars(ctx, q, variables)
+	if err != nil || resp == nil {
+		log.Fatal(err)
+	}
+
+}
+
+// CreateUserQuery creates a node
+func CreateUserQuery(ctx context.Context, dg *dgo.Dgraph) {
 
 	txn := dg.NewTxn()
 	defer txn.Discard(ctx)
@@ -123,8 +173,8 @@ func CreateQuery(ctx context.Context, dg *dgo.Dgraph) {
 	}
 
 	variables := map[string]string{"$id": assigned.Uids["user"]}
-	q := `query createUser($id: string){
-		create(func: uid($id)){
+	q := `query getUser($id: string){
+		get(func: uid($id)){
 			name
 			role
 			voertuig {
@@ -152,8 +202,39 @@ func CreateQuery(ctx context.Context, dg *dgo.Dgraph) {
 
 }
 
+// SearchUserQuery searches a node
+func SearchUserQuery(ctx context.Context, dg *dgo.Dgraph, id string) {
+	variables := map[string]string{"$id": id}
+	q := `query getUser($id: string){
+		get(func: uid($id)){
+			name
+			role
+			voertuig {
+				type
+				latitude
+				longitude
+				service {
+					needsservice
+				}
+			}
+		}
+	}`
+
+	resp, err := dg.NewTxn().QueryWithVars(ctx, q, variables)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.Unmarshal(resp.Json, &resp)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(resp.Json))
+}
+
 // SearchQuery searches nodes
-func SearchQuery(ctx context.Context, dg *dgo.Dgraph) {
+func SearchAllUsersQuery(ctx context.Context, dg *dgo.Dgraph) {
 
 	txn := dg.NewTxn()
 	defer txn.Discard(ctx)
@@ -163,7 +244,14 @@ func SearchQuery(ctx context.Context, dg *dgo.Dgraph) {
 		users(func: has(name)) {
 		  name
 		  role
-		  voertuig
+		  voertuig {
+			  type
+			  latitude
+			  longitude
+			  service {
+				  needsservice
+			  }
+		  }
 		}
 	}`
 
@@ -174,27 +262,3 @@ func SearchQuery(ctx context.Context, dg *dgo.Dgraph) {
 		log.Fatal(err)
 	}
 }
-
-// Schema voor dgraph:
-
-// name: string @index(exact) .
-// role: string .
-// needsservice: bool .
-// type: string .
-// latitude: float .
-// longitude: float .
-
-// type Service {
-// 	needsservice
-// }
-
-// type Voertuig {
-// 	type
-// 	latitude
-// 	longitude
-// }
-
-// type User {
-// 	name
-// 	role
-// }
